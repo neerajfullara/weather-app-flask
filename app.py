48c0779621d1f99dd4eb30682eff3f4a1b040efb
neerajfullara <fullaraneeraj@gmail.com>
2024-07-24 23:07:38 +0530

    Added the cities into the database and changed the code to take userd input to get cities

--- app.py
+++ app.py
@@ -1,25 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import requests
+import os
 
 app = Flask(__name__)
 app.config['DEBUG']=True
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+
+db = SQLAlchemy(app)
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/', methods=['GET','POST'])
 def index():
 
-    city='Las Vegas'
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=1e9dd8dfac35cb3e8b7090f24c07f3e3"
+    if request.method == 'POST':
+        new_city = request.form.get('city')
 
-    r=requests.get(url.format(city)).json()
+        if new_city:
+            new_city_obj = City(name=new_city)
+            db.session.add(new_city_obj)
+            db.session.commit()
 
-    weather={
-        'city': city,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon']
-    }
+    cities = City.query.all()
+    weather_data=[]
 
-    print(weather)
+    # city = 'Las Vegas'
+    for city in cities:
+        url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=1e9dd8dfac35cb3e8b7090f24c07f3e3"
+
+        r=requests.get(url.format(city.name)).json()
+
+        weather={
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon']
+        }
+        weather_data.append(weather)
 
 
-    return render_template('weatherApp.html', weather=weather)
+    return render_template('weatherApp.html', weather_data=weather_data)
